41cd5492e6463e13c887cf1283c686a51d82d429
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-12-14 13:34:04 +0300

    Первый

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # js-wrapper-lib
-javascript helpers, wrappers for core functions
+
+* eng: javascript helpers, wrappers for core functions
+* ru: Библиотека удобных оберток для стандартных функций javascript
+
+## Usage | Использование
+
+Add `src/jswl.js` to your html, export other way and call needle function like:
+
+```javascript
+jswl.functionName();
+```
+-- подключите `src/jswl.js` на html странице или иным образом добавьте в проект
+ и вызываейте нужную вам функцию, например (*for example*):
+
+```javascript
+jswl.isEmpty(value);
+```
+
---
+++ src/jswl.js
@@ -0,0 +1,106 @@
+var jswl = new JSWrapperLib();
+
+/**
+ * Набор обёрток, с более удобным синтаксисо
+ * (чистый JavaScript)
+ * 
+ * Если объявить точку входа как:
+ *   var jswl = new JSWrapperLib();
+ * 
+ * То функции в вашем скрипте можно вызывать как:
+ * 
+ * jswl.имяфункции()
+ * 
+ * @returns {undefined}
+ */
+function JSWrapperLib() {
+
+
+    var self = this;
+
+    /**
+     * Проверит является ли 
+     * значение null или undefined
+     * 
+     * @param {mixed} value  проверяемое значение
+     * @returns {boolean}
+     */
+    this.isNullOrUndefined = function(value)
+    {
+        return (typeof value === "undefined" 
+                || value === null);
+    }
+
+
+    /**
+     * Проверит содержится ли элемент в массиве
+     * 
+     * @param {mixed} value
+     * @param {array} array
+     * @returns {Boolean}
+     */
+    this.inArray = function(value, array)
+    {
+        return (!(array.indexOf(value) === -1));
+    }
+
+    /**
+     * Проверка на пустоту
+     * 
+     * @param {mixed} value
+     * @returns {Boolean}  проверяемое значение
+     */
+    this.isEmpty = function(value) {
+        return (typeof value === "undefined" || value === null || value ===  "");
+    }
+
+    /**
+     * Получит фрагмент строки фрагмент, если её части разделены квадратными скобками:
+     * people[123][groups][34][2]
+     * -- для номера 3 вернёт 34
+     * 
+     * @param {string} str  строка с фрагментами. окружеными квадратными скобками
+     * @param {int} number  номер фрагмента (начиная с нуля)
+     * @returns {string}
+     */
+    this.getSquareBracketedFragmentByNumber = function(str, number) {
+
+        var nameFrags = self.getSquareBracketedFragments(str);
+        return nameFrags[number];
+    }
+
+
+    /**
+     * Получит фрагментs строки фрагмент, если её части 
+     * разделены квадратными скобками в виде массива,
+     *  например дпя:
+     * people[123][groups][34][2]
+     * вернёт массив элементов (строк):
+     * [people, 123, groups, 34, 2]
+     * -- по факту разбиение идёт по открывающей скобке
+     * 
+     * @param {string} str  строка с фрагментами. окружеными квадратными скобками
+     * @returns {array} массив строк
+     */
+    this.getSquareBracketedFragments = function(str) {
+
+        var nameFrags = str.split('['); // разбиваем по открывающей скобке
+        nameFrags.forEach(function(element, index, nameFrags) {
+            nameFrags[index] = element.replace(/\]/g, ""); 
+        });
+        return nameFrags;
+    }
+
+    /**
+     * Проверит, что подстрока входит в данную строку
+     * (содержится в строке) 
+     * 
+     * @param {string} str    строка
+     * @param {string} substr  подстрока
+     * @returns {Boolean}
+     */
+    this.checkForSubstring = function(str, substr)
+    {
+        return (str.indexOf(substr) !== -1);
+    }
+}
