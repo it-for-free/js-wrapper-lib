1b188c48af21d54f5cd622ac5ca3fe55d5b7b0e9
vedro-compota <ru-ibn-hottab@yandex.ru>
2019-01-22 18:47:14 +0300

    Релиз: с минификацией и без

--- dist/jswl.js
+++ dist/jswl.js
@@ -1,1 +1,244 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.jswl=t():e.jswl=t()}(window,function(){return function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";n.r(t),t.default=new function(){var e=this;this.isNullOrUndefined=function(e){return null==e},this.inArray=function(e,t){return!(-1===t.indexOf(e))},this.isEmpty=function(e){return null==e||""===e||0===e},this.isDefined=function(e){return null!=e},this.getSquareBracketedFragmentByNumber=function(t,n){return e.getSquareBracketedFragments(t)[n]},this.getSquareBracketedFragments=function(e){var t=e.split("[");return t.forEach(function(e,t,n){n[t]=e.replace(/\]/g,"")}),t},this.checkForSubstring=function(e,t){return-1!==e.indexOf(t)},this.hello=function(){console.log("Hello JSWL! ;)")}}}]).default});
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["jswl"] = factory();
+	else
+		root["jswl"] = factory();
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/**
+ * jswl
+ */
+
+/**
+ * Набор обёрток, с более удобным синтаксисо
+ * (чистый JavaScript)
+ * 
+ * Если объявить точку входа как:
+ *   var jswl = new JSWrapperLib();
+ * 
+ * То функции в вашем скрипте можно вызывать как:
+ * 
+ * jswl.имяфункции()
+ * 
+ * @returns {undefined}
+ */
+function JSWrapperLib() {
+
+
+    var self = this;
+
+    /**
+     * Проверит является ли 
+     * значение null или undefined
+     * 
+     * @param {mixed} value  проверяемое значение
+     * @returns {boolean}
+     */
+    this.isNullOrUndefined = function(value)
+    {
+        return (typeof value === "undefined" 
+                || value === null);
+    }
+
+
+    /**
+     * Проверит содержится ли элемент в массиве
+     * 
+     * @param {mixed} value
+     * @param {array} array
+     * @returns {Boolean}
+     */
+    this.inArray = function(value, array)
+    {
+        return (!(array.indexOf(value) === -1));
+    }
+
+    /**
+     * Проверка на пустоту, пусто если:
+     *  - тип = "undefined"
+     *  - = null
+     *  - = ноль
+     *  - = пустой строке
+     * 
+     * @param {mixed} value  проверяемое значение
+     * @returns {Boolean} 
+     */
+    this.isEmpty = function(value) {
+        return (typeof value === "undefined" || value === null 
+            || value ===  "" || value ===  0);
+    }
+    
+    /**
+     * Определено ли значение:
+     * - тип не "undefined"
+     * - не = null
+     * 
+     * @param {mixed} value  проверяемое значение
+     * @returns {Boolean} 
+     */
+    this.isDefined = function(value) {
+        return (typeof value !== "undefined" && value !== null);
+    }
+
+    /**
+     * Получит фрагмент строки фрагмент, если её части разделены квадратными скобками:
+     * people[123][groups][34][2]
+     * -- для номера 3 вернёт 34
+     * 
+     * @param {string} str  строка с фрагментами. окружеными квадратными скобками
+     * @param {int} number  номер фрагмента (начиная с нуля)
+     * @returns {string}
+     */
+    this.getSquareBracketedFragmentByNumber = function(str, number) {
+
+        var nameFrags = self.getSquareBracketedFragments(str);
+        return nameFrags[number];
+    }
+
+
+    /**
+     * Получит фрагмент строки, если её части 
+     * разделены квадратными скобками в виде массива,
+     *  например дпя:
+     * people[123][groups][34][2]
+     * вернёт массив элементов (строк):
+     * [people, 123, groups, 34, 2]
+     * -- по факту разбиение идёт по открывающей скобке
+     * 
+     * @param {string} str  строка с фрагментами. окружеными квадратными скобками
+     * @returns {array} массив строк
+     */
+    this.getSquareBracketedFragments = function(str) {
+
+        var nameFrags = str.split('['); // разбиваем по открывающей скобке
+        nameFrags.forEach(function(element, index, nameFrags) {
+            nameFrags[index] = element.replace(/\]/g, ""); 
+        });
+        return nameFrags;
+    }
+
+    /**
+     * Проверит, что подстрока входит в данную строку
+     * (содержится в строке) 
+     * 
+     * @param {string} str    строка
+     * @param {string} substr  подстрока
+     * @returns {Boolean}
+     */
+    this.checkForSubstring = function(str, substr)
+    {
+        return (str.indexOf(substr) !== -1);
+    }
+    
+    /**
+     * Тестовый вызов jswl (привет мир) 
+     * Test jswl exists
+     *   
+     * @returns {undefined}
+     */
+    this.hello = function()
+    {
+        console.log('Hello JSWL! ;)');
+    }
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (new JSWrapperLib());
+
+/***/ })
+/******/ ])["default"];
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,42 @@
 "use strict";
 var path = require('path'); // для работы с path
 
-module.exports = {
-  mode: 'production',
-  entry: {
-      'jswl': './src/jswl.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    library: 'jswl',
-    libraryExport: 'default',
-    libraryTarget: 'umd'
-  },
-  watchOptions: {
-      aggregateTimeout: 500,
-      poll: 1000 // порверяем измемения раз в секунду
-  }
-  
-};
+module.exports = [
+    {
+        mode: 'production',
+        entry: {
+            'jswl': './src/jswl.js'
+        },
+        output: {
+          path: path.resolve(__dirname, 'dist'),
+          filename: '[name]-min.js',
+          library: 'jswl',
+          libraryExport: 'default',
+          libraryTarget: 'umd'
+        },
+        watchOptions: {
+            aggregateTimeout: 500,
+            poll: 1000 // порверяем измемения раз в секунду
+        }
+    },
+    {  // без минификации
+        mode: 'production',
+        entry: {
+            'jswl': './src/jswl.js'
+        },
+        output: {
+          path: path.resolve(__dirname, 'dist'),
+          filename: '[name].js',
+          library: 'jswl',
+          libraryExport: 'default',
+          libraryTarget: 'umd'
+        },
+        watchOptions: {
+            aggregateTimeout: 500,
+            poll: 1000 // порверяем измемения раз в секунду
+        },
+        optimization: {
+            minimize: false
+        }
+    },
+];
