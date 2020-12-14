66a5f6326a9a6d8edde166913c1249c5c57419f2
vedro-compota <ru-ibn-hottab@yandex.ru>
2020-12-14 16:40:39 +0300

    Добавляем доп. юнит-тесты.

--- __tests__/array/countSubElementsWithValue.test.js
+++ __tests__/array/countSubElementsWithValue.test.js
@@ -1,7 +1,6 @@
 
 import jswl from '../../src/jswl.js';
 
-
 let arr = [
     {
 	sections: [
@@ -13,7 +12,8 @@ let arr = [
 	    },
 	    {
 		floors: [
-		    {id: 2, value: 1, open: true}
+		    {id: 2, value: 1, open: true},
+		    {id: 2, value: 1, open: true},
 		],
 	    },
 	    {
@@ -47,17 +47,21 @@ let arr = [
     },
 ];
 
-test("for 'sections[]floors[].value', 1  returns 3", () => {
-  expect(jswl.arr.countSubElementsWithValue(arr, 'sections[]floors[].value', 1)).toBe(4);
+test("for 'sections[]floors[].value', 1  returns 5", () => {
+  expect(jswl.arr.countSubElementsWithValue(arr, 'sections[]floors[].value', 1)).toBe(5);
 });
 
-test("for 'sections[]floors[].open', 1  returns 3", () => {
-  expect(jswl.arr.countSubElementsWithValue(arr, 'sections[]floors[].open', true)).toBe(3);
+test("for 'sections[]floors[].open', true returns 4", () => {
+  expect(jswl.arr.countSubElementsWithValue(arr, 'sections[]floors[].open', true)).toBe(4);
 });
 
 
-test("for subarray 'arr[0].sections' path 'floors[].value', 1  returns 3", () => {
-  expect(jswl.arr.countSubElementsWithValue(arr[0].sections, 'floors[].open', true)).toBe(2);
+test("for subarray 'arr[0].sections' path floors[].open', true  returns 3", () => {
+  expect(jswl.arr.countSubElementsWithValue(arr[0].sections, 'floors[].open', true)).toBe(3);
+});
+
+test("for subarray 'arr[0].sections[0].floors' path 'open', true  returns 1", () => {
+  expect(jswl.arr.countSubElementsWithValue(arr[0].sections[0].floors, 'open', true)).toBe(1);
 });
 
 
